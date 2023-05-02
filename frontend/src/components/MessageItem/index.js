483602676c045740ed1bd4d257346076e5d5a6f8
Ryder_Aguilera <ryderaguilera@hotmail.com>
2023-05-02 11:39:30 -0700

    Message Box Hover

--- frontend/src/components/MessageItem/index.js
+++ frontend/src/components/MessageItem/index.js
@@ -1,11 +1,17 @@
 import "./MessageItem.css"
 import * as utilActions from "../../util/consumer"
+import { AiOutlineClose } from "react-icons/ai";
+import { BsFillPencilFill } from "react-icons/bs";
+import { useState } from "react";
 
 function MessageItem ({message}) {
     console.log(message,"message before display")
     let date = utilActions.dateFormat(message.createdAt)
+    const [hover,setHover] = useState(false);
+
     return (
-        <div className="message">
+        <div className="message" onMouseEnter={()=>{setHover(true)}} onMouseLeave={()=>{setHover(false)}}>
+            <div className="messageTooltip" style={{display: hover ? "inline-block" : "none"}}><BsFillPencilFill size={20}/> <AiOutlineClose size={20}/></div>
             <div className="messageTop">
             <div className="messageUser">{`${message.username}`}</div>
             <div className="messageDate">{`${date}`}</div>
