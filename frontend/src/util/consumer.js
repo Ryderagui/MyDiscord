7f98989de2357ef9910d80beea84442f09c0c5bf
Ryder_Aguilera <ryderaguilera@hotmail.com>
2023-05-04 14:10:41 -0700

    Small CSS changes

--- frontend/src/util/consumer.js
+++ frontend/src/util/consumer.js
@@ -2,7 +2,7 @@ import {createConsumer} from '@rails/actioncable'
 
 
 let url = "ws://localhost:5000/cable"
-if (process.env.NODE_ENV !== 'production'){
+if (process.env.NODE_ENV === 'production'){
     url = process.env.REDIS_URL;
 }
 const consumer = createConsumer(url)
@@ -14,6 +14,6 @@ export const dateFormat = (date)=>{
     let year = date.substring(0,4);
     let month = date.substring(5,7);
     let day = date.substring(8,10);
-    let time = date.substring(12,17);
+    let time = date.substring(12,16);
     return `${month}/${day}/${year} ${time}`
 }
