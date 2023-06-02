a9ca59174928fc9f2563d17ea338ffd0cae0c6d6
Ryder_Aguilera <ryderaguilera@hotmail.com>
2023-06-01 19:35:51 -0700

    Try again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,5 +87,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_cable.disable_request_forgery_protection = true
-  config.web_socket_server_url = "wss://revel-yz0c.onrender.com/cable" 
 end
